2a2405f8ef0acfaf40d6622460df174dd12f04c7
like-rounak <rounaks.it.ug@jadavpuruniversity.in>
2024-03-17 11:57:07 +0530

    React-DOM + Features

--- fridge-vision/src/App.jsx
+++ fridge-vision/src/App.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navigation } from "./components/navigation";
 import { Header } from "./components/header";
 import { Features } from "./components/features";
+import UploadForm from './components/UploadForm'; // Import UploadForm
 import { About } from "./components/about";
 import { Services } from "./components/services";
 import { Gallery } from "./components/gallery";
@@ -13,29 +15,34 @@ import SmoothScroll from "smooth-scroll";
 import "./App.css";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
-  speedAsDuration: true,
+    speed: 1000,
+    speedAsDuration: true,
 });
 
 const App = () => {
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
+    const [landingPageData, setLandingPageData] = useState({});
+    useEffect(() => {
+        setLandingPageData(JsonData);
+    }, []);
 
-  return (
-    <div>
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery} />
-      <Testimonials data={landingPageData.Testimonials} />
-      <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Navigation />
+                <Header data={landingPageData.Header} />
+                <Routes>
+                    <Route path="/" element={<Features data={landingPageData.Features} />} />
+                    <Route path="/uploadform" element={<UploadForm />} />
+                </Routes>
+                <About data={landingPageData.About} />
+                <Services data={landingPageData.Services} />
+                <Gallery data={landingPageData.Gallery} />
+                <Testimonials data={landingPageData.Testimonials} />
+                <Team data={landingPageData.Team} />
+                <Contact data={landingPageData.Contact} />
+            </div>
+        </Router>
+    );
 };
 
 export default App;
--- fridge-vision/src/components/features.jsx
+++ fridge-vision/src/components/features.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export const Features = (props) => {
+export const Features = () => {
+  const navigate = useNavigate();
+  const featuresData = [
+    {
+      icon: "fa fa-comments-o",
+      title: "Fridge Vision",
+      text: "Fridge Vision is a web application that allows users to take a picture of the inside of their fridge and receive a list of recipes that can be made with the ingredients they have.",
+      onClick: () => navigate('/UploadForm.jsx')
+    },
+    {
+      icon: "fa fa-bullhorn",
+      title: "Lorem ipsum",
+      text: "Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend pellentesque natoque etiam."
+    },
+    {
+      icon: "fa fa-group",
+      title: "Lorem ipsum",
+      text: "Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend pellentesque natoque etiam."
+    },
+    {
+      icon: "fa fa-magic",
+      title: "Lorem ipsum",
+      text: "Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend pellentesque natoque etiam."
+    }
+  ];
+
   return (
     <div id="features" className="text-center">
       <div className="container">
@@ -8,18 +34,18 @@ export const Features = (props) => {
           <h2>Features</h2>
         </div>
         <div className="row">
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
-                  {" "}
-                  <i className={d.icon}></i>
-                  <h3>{d.title}</h3>
-                  <p>{d.text}</p>
-                </div>
-              ))
-            : "Loading..."}
+          {featuresData.map((d, i) => (
+            <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3" onClick={d.onClick}>
+              {" "}
+              <i className={d.icon}></i>
+              <h3>{d.title}</h3>
+              <p>{d.text}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
 };
+
+export default Features;
